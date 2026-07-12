7f169337573a4e82a512c63dade88cc267190535
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-13 08:43:51 +0900

    Clean up twrp_ctl2.mk by removing comments
    Removed copyright notices and redundant inherit-product calls.

--- twrp_ctl2.mk
+++ twrp_ctl2.mk
@@ -1,17 +1,3 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-$(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, device/benesse/ctl2/device.mk)
-
 PRODUCT_DEVICE := ctl2
 PRODUCT_NAME := twrp_ctl2
 PRODUCT_BRAND := Benesse
@@ -24,3 +10,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="a733_pro3_arm64-user 14 UP1A.231105.001.A1 20260414 release-keys"
 
 BUILD_FINGERPRINT := Benesse/50S904/50S904:15/AP3A.241105.008/20260414:user/release-keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, device/benesse/ctl2/device.mk)
