573ac6a83a4d2a9b76bb726cfe0948f89f71eb8b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-13 22:13:21 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,9 +1,2 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The Team Win Recovery Project
-#
-
-
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
