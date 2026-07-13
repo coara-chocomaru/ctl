95162538d152cc0dd36eeb8e98fa331aef557e64
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-13 13:33:02 +0900

    Fix case sensitivity in product makefile names

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,6 +3,6 @@
 # Copyright (C) 2023 The Team Win Recovery Project
 #
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_ctl2.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_CTL2.mk
 
-COMMON_LUNCH_CHOICES := twrp_ctl2-eng
+COMMON_LUNCH_CHOICES := twrp_CTL2-eng
