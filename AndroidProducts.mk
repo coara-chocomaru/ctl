5b749ce538bc5d2ea33ce408c3c4db50def2289f
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-14 17:20:46 +0900

    Update PRODUCT_MAKEFILES path in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,4 +3,4 @@
 # Copyright (C) 2023 The Team Win Recovery Project
 #
 DEVICE_PATH := device/benesse/ctl2
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_CTL2.mk
+PRODUCT_MAKEFILES := $(DEVICE_PATH)/twrp_CTL2.mk
