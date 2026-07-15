21cad085951c9dfbffe03e412a5e2aa56d4437b3
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-16 04:16:55 +0900

    Clean up inherit-product calls in twrp_CTL2.mk
    Removed unnecessary inherit-product calls for core 64-bit and virtual AB OTA.

--- twrp_CTL2.mk
+++ twrp_CTL2.mk
@@ -1,4 +1,3 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 PRODUCT_RELEASE_CONFIG_MAPS += device/benesse/ctl2/release/release_config_map.textproto
 $(call inherit-product, device/benesse/ctl2/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -19,6 +18,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=CTL2 \
     BUILD_PRODUCT=CTL2 \
     TARGET_DEVICE=CTL2
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
